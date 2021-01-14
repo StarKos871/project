c8032e6ef73618c45d265a1b16e37b42bb66d38b
Kostya <kos871@mail.ru>
2021-01-14 14:49:54 +0300

    add cycles

--- script.js
+++ script.js
@@ -23,16 +23,38 @@
 // //  иначе (:) другое
 // (num == 50) ? console.log('Ok!') : console.log('Error');
 
-const mun = 50
+// const mun = 50
 
-switch (mun) {
-  case 49:
-    console.log("No!")
-    break
-  case 50:
-    console.log('Yes!!!')
-    break
-  default:
-    console.log('In second day')
-    break
+// switch (mun) {
+//   case 49:
+//     console.log("No!")
+//     break
+//   case 50:
+//     console.log('Yes!!!')
+//     break
+//   default:
+//     console.log('In second day')
+//     break
+// }
+
+// Циклы
+
+// let num = 50
+
+// // while (num < 55) {
+// //   console.log(num)
+// //   num++
+// // }
+
+// for (i = 0; i <= 10; i++){
+//   console.log(num)
+//   num++
+// } 
+
+for (i = 0; i < 10; i++){
+  if (i === 6){
+    // break   (этот операор позволяет остановить цикл при выполнении определенного уловия)
+    continue  // (этот оператор позволяет продолжить выполнение цикла, но без цифры прописанной в условии)
+  }
+  console.log(i);
 }
