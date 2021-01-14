a12f0b1c03cdecc898d30483bf848c9fa5816f3c
Kostya <kos871@mail.ru>
2021-01-14 14:24:12 +0300

    in scipt.js add conditions

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '')
+// const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '')
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  private: false,
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   private: false,
+// }
+
+// const a = prompt('Один из просмотренных вами фильмов?', ''),
+//       b = prompt('На сколько оцните его?', ''),
+//       c = prompt('Один из просмотренных вами фильмов?', ''),
+//       d = prompt('На сколько оцните его?', '')
+
+// personalMovieDB.movies[a] = b
+// personalMovieDB.movies[c] = d
+
+// console.log(personalMovieDB);
+
+// const  num = 50;
+// // Тернарный  оператор: форма записи внизу. Если переменная = чему-то, то (?) какое то действие,
+// //  иначе (:) другое
+// (num == 50) ? console.log('Ok!') : console.log('Error');
+
+const mun = 50
+
+switch (mun) {
+  case 49:
+    console.log("No!")
+    break
+  case 50:
+    console.log('Yes!!!')
+    break
+  default:
+    console.log('In second day')
+    break
 }
-
-const a = prompt('Один из просмотренных вами фильмов?', ''),
-      b = prompt('На сколько оцните его?', ''),
-      c = prompt('Один из просмотренных вами фильмов?', ''),
-      d = prompt('На сколько оцните его?', '')
-
-personalMovieDB.movies[a] = b
-personalMovieDB.movies[c] = d
-
-console.log(personalMovieDB);
